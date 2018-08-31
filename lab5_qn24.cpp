1f44e58c15bbdb13aba33f98f70a28f3ea29300f
priyasahu22 <42238834+priyasahu22@users.noreply.github.com>
2018-08-31 02:06:25 -0400

    Add files via upload

---
+++ lab5_qn24.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//print alphabatical  character from a to z
+int main()
+{ //int a;
+  //type casting
+  float b=float(2)/5;
+ cout<<b;
+  cout<<" "<<int('A');
+  cout<<" "<<int('A');
+  cout<<" "<<int('A');
+  cout<<" "<<int('A');
+
+  
+return 0;
+}
